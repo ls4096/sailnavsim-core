5a0ef64c7f231c875ebb8b47ba5566bce3352e2b
ls4096 <ls4096@8bitbyte.ca>
2020-08-12 20:09:24 -0400

    Use latest libproteus version, and make this SailNavSim version 1.2.0.

--- src/main.c
+++ src/main.c
@@ -59,7 +59,7 @@
 #define SQLITE_DB_FILENAME "./sailnavsim.sql"
 
 
-static const char* VERSION_STRING = "SailNavSim version 1.2.0-dev (" __DATE__ " " __TIME__ ")";
+static const char* VERSION_STRING = "SailNavSim version 1.2.0 (" __DATE__ " " __TIME__ ")";
 
 
 static int parseArgs(int argc, char** argv);
